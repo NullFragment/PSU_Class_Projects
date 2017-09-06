2ec9323cb4ca5d3904816c9d2c770b35b77ad955
Kyle Salitrik <nullfragment@gmail.com>
2017-09-06 16:21:40 -0400

    CMPSC 450 - Assignment 2 - Begun Optimization

--- CMPSC_450/Assignment_2/main.cpp
+++ CMPSC_450/Assignment_2/main.cpp
@@ -1,74 +1,66 @@
 // compiles with:
 // g++ slow_code.cpp -o slow_code
 
-
 #include <sys/time.h>
-#include <stdio.h>
-#include <stdlib.h>
 #include <vector>
-#include <math.h>
+#include <map>
+#include <cmath>
 
-
-void get_walltime(double* wcTime) {
-
+void get_walltime(double *wcTime)
+{
     struct timeval tp;
-
     gettimeofday(&tp, NULL);
-
-    *wcTime = (double)(tp.tv_sec + tp.tv_usec/1000000.0);
-
+    *wcTime = (double) (tp.tv_sec + tp.tv_usec / 1000000.0);
 }
 
 // complex algorithm for evaluation
 void myfunc(std::vector<std::vector<double> > &v_s,
-            std::vector<std::vector<double> > &v_mat, std::vector<int> &i_v)
+            std::vector<std::vector<double> > &v_mat, std::vector<int> &i_v,
+            std::map<int, double> &value_map)
 {
-    // this assumes that the two dimensional vector is square
-
-    double d_val;
-
-    for (int j = 0; j < v_s.size(); j ++)
+    int d_val;
+    for (int j = 0; j < v_s.size(); j++)
     {
         for (int i = 0; i < v_s[0].size(); i++)
         {
-            d_val = round(fmod(i_v[i],256)); // this should return an integer
-            v_mat[i][j] = v_s[i][j]*(sin(d_val)*sin(d_val)-cos(d_val)*cos(d_val));
+            d_val = i_v[i] % 256;
+            v_mat[i][j] = v_s[i][j] * (value_map[d_val]);
         }
     }
 }
 
 int main(int argc, char *argv[])
 {
-
     // this should be called as> ./slow_code <i_R> <i_N>
-
     int i_R = 1000;
     int i_N = 100;
 
     double d_S, d_E;
-
     // parse input parameters
     if (argc >= 2)
     {
         i_R = atoi(argv[1]);
     }
-
     if (argc >= 3)
     {
         i_N = atoi(argv[2]);
     }
 
     // some declarations
-    std::vector<std::vector<double> > vd_s(i_N, std::vector<double>(i_N) );
-    std::vector<std::vector<double> > vd_mat(i_N, std::vector<double>(i_N) );
+    std::vector<std::vector<double> > vd_s(i_N, std::vector<double>(i_N));
+    std::vector<std::vector<double> > vd_mat(i_N, std::vector<double>(i_N));
     std::vector<int> vi_v(i_N);
-
+    std::map<int, double> m_difference;
     // populate memory with some random data
     for (int i = 0; i < i_N; i++)
     {
         vi_v[i] = i * i;
+        m_difference[(i * i) % 256] = sin((i * i) % 256) * sin((i * i) % 256)
+                                      - cos((i * i) % 256) * cos((i * i) % 256);
         for (int j = 0; j < i_N; j++)
+        {
             vd_s[i][j] = j + i;
+        }
     }
 
     // start benchmark
@@ -77,7 +69,7 @@ int main(int argc, char *argv[])
     // iterative test loop
     for (int i = 0; i < i_R; i++)
     {
-        myfunc(vd_s, vd_mat, vi_v);
+        myfunc(vd_s, vd_mat, vi_v, m_difference);
     }
 
     // end benchmark
