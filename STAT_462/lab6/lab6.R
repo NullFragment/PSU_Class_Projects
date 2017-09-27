0809e2e4732f70206316d792e4551e93577ac1b5
Kyle Salitrik <nullfragment@gmail.com>
2017-09-27 18:23:37 -0400

    STAT462 - Finished lab 6

--- STAT_462/lab6/lab6.R
+++ STAT_462/lab6/lab6.R
@@ -1,48 +1,28 @@
 bear=read.table('bears.txt',header=TRUE,sep='\t')
 bear=bear[bear$Obs.No==1,]
 
+## Linear System Solvera
+lm_six = lm(bear$Weight~bear$Head.L+bear$Head.W+bear$Neck.G+bear$Length+bear$Chest.G)
+RSS_six = sum(lm_six$residuals^2)
+
+
+lm_reduced = lm(bear$Weight~bear$Head.L+bear$Neck.G+bear$Chest.G)
+RSS_reduced = sum(lm_reduced$residuals^2)
+
+
 ## Set useful variables
 n = dim(bear)[1]
-p = 2
-ybar = mean(bear$Weight)
-y=bear$Weight
-X1=as.matrix(cbind(1,bear$Neck.G))
+p=length(lm_six$coefficients)
+q=length(lm_reduced$coefficients)
+f=((RSS_reduced-RSS_six)/(p-q))/(RSS_six/(n-p))
+pvalue=pf(f,p-q,n-p,lower.tail=FALSE)
 
-## Solve for linear coefficients
-beta1=solve(t(X1)%*%X1,t(X1)%*%y)
 
-## Compute residual sum of squares and total sum of squares
-RSS1 = sum((y - X1%*%beta1)^2)
-TSS = sum((y-ybar)^2)
-SS_reg = sum((X1%*%beta1 - ybar)^2)
-
-## Compute variance
-variance = (1/(n-p))*RSS1
-std_dev = sqrt(variance)
-
-## Compute variance of errors
-inv = solve(t(X1)%*%X1)
-var_error = sqrt(variance*diag(inv))
-
-## Perform T statistic test
-t = beta1/var_error
-pvalue=2*pt(abs(t),df=n-p,lower.tail=FALSE)
-
-## Compute R
-R1 = 1-RSS1/TSS
-
-## Display Values
-TSS - (RSS1 + SS_reg)
-var_error
-t
+## Display Results
+summary(lm_six)
+summary(lm_reduced)
+RSS_six
+RSS_reduced
+f
 pvalue
-
-
-
-## Linear System Solver
-lm_neck = lm(bear$Weight~bear$Neck.G)
-summary(lm_neck)
-
-## Plot linear solved system
-plot(bear$Neck.G,bear$Weight)
-abline(beta1[1],beta1[2],col='red')
+anova(lm_reduced,lm_six)
