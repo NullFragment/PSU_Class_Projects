de855d8bdd23cdeb0739ae46000638d4c9bfb586
Kyle Salitrik <nullfragment@gmail.com>
2017-11-29 23:38:39 -0500

    STAT462 - Project - Completed through model selection and VIF

--- STAT_462/project/project.R
+++ STAT_462/project/project.R
@@ -1,17 +1,16 @@
 ####################################################################
 #### Setup
 ####################################################################
-## Install libraries (Uncomment to install)
-#install.packages('reshape2')
-#install.packages('ggplot2')
-#install.packages('gridExtra')
-#install.packages('leaps')
+## Install and load libraries
+ipak <- function(pkg){
+  new.pkg <- pkg[!(pkg %in% installed.packages()[, "Package"])]
+  if (length(new.pkg)) 
+    install.packages(new.pkg, dependencies = TRUE)
+  sapply(pkg, require, character.only = TRUE)
+}
 
-## Import needed libraries and data
-require(reshape2)
-require(ggplot2)
-require(gridExtra)
-require(leaps)
+packages <- c("ggplot2", "reshape2", "gridExtra", "leaps", "car")
+ipak(packages)
 
 ## Load and label data
 baseball = read.table("baseball.dat.txt")
@@ -30,83 +29,230 @@ names(baseball) <-
   'StrikeOuts',
   'StolenBases',
   'Errors',
-  'FreeAgencyEligibility',
+  'FreAgtEli',
   'FreeAgent',
-  'ArbitrationEligibility',
+  'ArbEli',
   'Arbitration',
   'Name'
   )
-summary(baseball)
 
-## Remove double-counted data
-baseball$Hits = baseball$Hits - baseball$Doubles - baseball$Triples - baseball$HomeRuns
+## Add singles data
+baseball$Singles <- baseball$Hits - baseball$Doubles - baseball$Triples - baseball$HomeRuns
+
+## Reorder columns
+# baseball <- baseball[,c("Salary","BattingAverage","OBP","Runs","Hits","Singles","Doubles","Triples","HomeRuns","RBI","Walks","StrikeOuts","StolenBases","Errors","FreAgtEli",
+#                         "FreeAgent","ArbEli","Arbitration","Name")]
+
+## Remove measurement error
+baseball = subset(baseball, baseball$Singles > 0)
+
+## Display data summary without categorical variables
+summary(baseball[1:13])
 
 ## Compute Correlation Matrix
-corr_mat <- cor(baseball[,-18])
+corr_mat <- cor(baseball[1:17])
 
 ####################################################################
-#### Initial Linear Model Exploration
+#### Initial Data Exploration
 ####################################################################
-## Fit inital linear models
-lm_salary = lm(Salary ~ Runs + StrikeOuts + StolenBases + OBP, data = baseball)
-lm_salary_expanded = lm(Salary ~ Runs + StrikeOuts + StolenBases + Hits + Doubles + Triples + HomeRuns + Walks
-                        , data =baseball)
-lm_salary_full = lm(Salary ~ . - Name, data = baseball)
-
-## Create Scatter Plots
-## Use 1024x576 for image size
-subset_obp = baseball[, c('OBP', 'Hits', 'Doubles', 'Triples', 'HomeRuns', 'Walks')]
-subset_salary_expanded = baseball[, c('Salary', 'OBP', 'Runs', 'StrikeOuts', 'StolenBases')]
-
-// Salary Model
-p1 <- ggplot(baseball, aes(x = OBP, y = Salary)) + geom_point(size = 1)
-p2 <- ggplot(baseball, aes(x = Runs, y = Salary)) + geom_point(size = 1)
-p3 <- ggplot(baseball, aes(x = StrikeOuts, y = Salary)) + geom_point(size = 1)
-p4 <- ggplot(baseball, aes(x = StolenBases, y = Salary)) + geom_point(size = 1)
-grid.arrange(p1, p2, p3, p4, ncol = 2)
-
-
-p1 <- ggplot(baseball, aes(x = Hits, y = Salary)) + geom_point(size = 1)
-p2 <- ggplot(baseball, aes(x = Doubles, y = Salary)) + geom_point(size = 1)
-p3 <- ggplot(baseball, aes(x = Triples, y = Salary)) + geom_point(size = 1)
-p4 <- ggplot(baseball, aes(x = HomeRuns, y = Salary)) + geom_point(size = 1)
-p5 <- ggplot(baseball, aes(x = Walks, y = Salary)) + geom_point(size = 1)
-grid.arrange(p1, p2, p3, p4, p5, ncol = 3)
+## Create Scatter Plots of Data
+plot(baseball[1:13])
 
 ## Generate Histograms of data
-ggplot(data=melt(baseball[,-18]),aes(x = value)) + facet_wrap(~variable,scales = "free_x") + geom_histogram()
-
-
-## VIF on OBP Model
-X_obp = model.matrix(lm_obp)
-R2 = vector("numeric", length(lm_obp$coefficients) - 1)
-for (j in 1:length(lm_obp$coefficients) - 1) 
-{
-  y_tmp = X_obp[, 1 + j]
-  x_tmp = as.matrix(X_obp[, -c(1, 1 + j)])
-  lm_fit = lm(y_tmp ~ x_tmp)
-  R2[j] = summary(lm_fit)$r.squared
-}
-VIF_obp = 1 / (1 - R2)
-names(VIF_obp) = c('Hits', 'Doubles', 'Triples', 'HomeRuns', 'Walks')
-VIF_obp
+ggplot(data=melt(baseball[1:13]),aes(x = value)) + facet_wrap(~variable,scales = "free_x") + geom_histogram(bins = 50)
 
 ## Free un-necessary memory
-rm(p1, p2, p3, p4, p5)
-rm(subset_salary_expanded, subset_obp)
-rm(j, X_obp, x_tmp, y_tmp, R2)
-invisible(gc())
+# rm(p1, p2, p3, p4, p5)
+# rm(subset_salary_expanded, subset_obp)
+# rm(j, X_obp, x_tmp, y_tmp, R2)
+# invisible(gc())
 
 ####################################################################
-#### Model Selection Exploration
+#### Model Selection
 ####################################################################
+## Fit inital linear models
+lm_full = lm(Salary ~ . - Name - Singles, data = baseball)
+lm_red = lm(Salary ~ . - Name - OBP - Singles, data = baseball)
+
 ## Use LEAPS for Model Selection Exploration
-select_obp = regsubsets(OBP ~ Hits + Doubles + Triples + HomeRuns + Walks,method = "exhaustive",nbest = 1,data = baseball)
-select_salary_expanded = regsubsets(Salary ~ OBP + Runs + StrikeOuts + StolenBases,method = "exhaustive",nbest = 1,data = baseball)
-select_salary_full = regsubsets(Salary ~ . - Name,method = "exhaustive",nbest = 1,data = baseball)
+select_full = regsubsets(Salary ~ . - Name - Singles,method = "exhaustive",nbest = 1,nvmax=16,data = baseball)
+select_red = regsubsets(Salary ~ . - Name - Singles - OBP,method = "exhaustive",nbest = 1,nvmax=16,data = baseball)
+full_sum = summary(select_full)
+red_sum = summary(select_red)
 
 ## Display Data
-summary(select_obp)$which
-summary(select_salary_expanded)$which
-summary(select_salary_full)$which
+full_sum$which
+red_sum$which
+
+## Useful Values
+totalSS=sum((baseball$Salary-mean(baseball$Salary))^2)
+n=nrow(baseball)
+sigma_hat_full=summary(lm_full)$sigma
+
+####################################################################
+#### Full Model AIC, BIC, Mallows C, Adj R2
+####################################################################
+## Necessary Values
+p_full=17
+p_f=2:p_full
+RSS_full=full_sum$rss
+
+## Adjusted R2
+R2_adj_full=1-(RSS_full/(n-p_f))/(totalSS/(n-1))
+plot(p_f,R2_adj_full,xlab="Number of betas",ylab="Adjusted R-squared")
+
+## Mallow's C
+C_p_full=RSS_full/(sigma_hat_full^2)+2*p_f-n
+C_p_diff_full = abs(abs(C_p_full)-p_f)
+plot(p_f,C_p_full,xlab="Number of betas",ylab="Mallow's Cp")
+abline(0,1)
+
+## AIC
+aic_full=n*log(RSS_full/n)+2*p_f
+plot(p_f,aic_full,xlab="Number of betas",ylab="AIC")
+
+## BIC
+bic_full=n*log(RSS_full/n)+p_f*log(n)
+plot(p_f,bic_full,xlab="Number of betas",ylab="BIC")
+
+## Suggested Number of Betas
+r2_f = which.max(R2_adj_full)+1
+mc_f = which.min(C_p_diff_full[1:15])+1
+aic_f = which.min(aic_full)+1
+bic_f = which.min(bic_full)+1
+full_selection_betas = rbind(c(r2_f, mc_f, aic_f, bic_f))
+
+####################################################################
+#### Reduced Model AIC, BIC, Mallows C, Adj R2
+####################################################################
+## Necessary Values
+p_red = 16
+p_r=2:p_red
+RSS_red=red_sum$rss
+
+## Adjusted R2
+R2_adj_red=1-(RSS_red/(n-p_r))/(totalSS/(n-1))
+plot(p_r,R2_adj_red,xlab="Number of betas",ylab="Adjusted R-squared")
+
+## Mallow's C
+C_p_red=RSS_red/(sigma_hat_full^2)+2*p_r-n
+C_p_diff_red = abs(abs(C_p_red)-p_r)
+plot(p_r,C_p_red,xlab="Number of betas",ylab="Mallow's Cp")
+abline(0,1)
+
+## AIC
+aic_red=n*log(RSS_red/n)+2*p_r
+plot(p_r,aic_red,xlab="Number of betas",ylab="AIC")
+
+## BIC
+bic_red=n*log(RSS_red/n)+p_r*log(n)
+plot(p_r,bic_red,xlab="Number of betas",ylab="BIC")
+
+
+## Suggested Number of Betas
+r2_r = which.max(R2_adj_red)+1
+mc_r = which.min(C_p_diff_red[1:14])+1
+aic_r = which.min(aic_red)+1
+bic_r = which.min(bic_red)+1
+red_selection_betas = rbind(c(r2_r, mc_r, aic_r, bic_r))
+
+####################################################################
+#### Model Selection Summary and VIF Analysis
+####################################################################
+## Number of Betas suggested by each method
+selection_matrix = matrix(c(full_selection_betas,red_selection_betas),ncol=length(full_selection_betas))
+colnames(selection_matrix) <- c('Adj R2','MallowC','AIC','BIC')
+rownames(selection_matrix) <- c('Full','Reduced')
+
+## Full Model Selection Results
+lm_full_aic = lm(Salary ~ Runs+Hits+Triples+HomeRuns+RBI+StrikeOuts+StolenBases+Errors+FreAgtEli+FreeAgent+ArbEli+Arbitration, data=baseball)
+lm_full_bic = lm(Salary ~ Runs+Hits+HomeRuns+RBI+StrikeOuts+StolenBases+FreAgtEli+FreeAgent+ArbEli, data=baseball)
+
+## Reduced (No OBP) Model Selection Results
+lm_red_r2 = lm(Salary ~ Runs+Hits+HomeRuns+RBI+StrikeOuts+StolenBases+FreAgtEli+FreeAgent+ArbEli, data=baseball)
+lm_red_aic = lm(Salary ~ BattingAverage+Runs+Hits+Doubles+Triples+HomeRuns+RBI+StrikeOuts+StolenBases+Errors+FreAgtEli+FreeAgent+ArbEli+Arbitration, data=baseball)
+lm_red_bic = lm(Salary ~ HomeRuns+RBI+StrikeOuts+StolenBases+FreAgtEli+ArbEli, data=baseball)
+
+## NOTE: lm_full_bic and lm_red_r2 are the same model!
+
+######################
+# Full models
+######################
+## Full AIC VIF
+vif(lm_full_aic)
+which.max(vif(lm_full_aic))
+
+lm_full_aic = update(lm_full_aic, . ~ . - RBI) 
+vif(lm_full_aic)
+which.max(vif(lm_full_aic))
+
+lm_full_aic = update(lm_full_aic, . ~ . - Runs) 
+vif(lm_full_aic)
+
+## Full BIC VIF
+vif(lm_full_bic)
+which.max(vif(lm_full_bic))
+
+lm_full_bic = update(lm_full_bic, . ~ . - RBI) 
+vif(lm_full_bic)
+which.max(vif(lm_full_bic))
+
+lm_full_bic = update(lm_full_bic, . ~ . - Runs) 
+vif(lm_full_bic)
+
+######################
+# Reduced models
+######################
+## Reduced AIC VIF
+vif(lm_red_aic)
+which.max(vif(lm_red_aic))
+
+lm_red_aic = update(lm_red_aic, . ~ . - Hits) 
+vif(lm_red_aic)
+which.max(vif(lm_red_aic))
+
+lm_red_aic = update(lm_red_aic, . ~ . - RBI) 
+vif(lm_red_aic)
+which.max(vif(lm_red_aic))
+
+lm_red_aic = update(lm_red_aic, . ~ . - Runs) 
+vif(lm_red_aic)
+
+## Reduced BIC VIF
+vif(lm_red_bic)
+which.max(vif(lm_red_bic))
+
+lm_red_bic = update(lm_red_bic, . ~ . - RBI)
+vif(lm_red_bic)
+
+####################################################################
+#### Adjusted Models after VIF and final model selection
+####################################################################
+## After selection lm_red_r2 and lm_full_bic are the same
+summary(lm_full_aic) # Adj R2 Value = 0.6745
+summary(lm_full_bic) # Adj R2 Value = 0.6732
+summary(lm_red_aic) # Adj R2 Value = 0.6678
+summary(lm_red_bic) # Adj R2 Value = 0.6459
+
+adj_r2_sum = c(summary(lm_full_aic)$adj.r.squared,summary(lm_full_bic)$adj.r.squared,summary(lm_red_aic)$adj.r.squared,summary(lm_red_bic)$adj.r.squared)
+names(adj_r2_sum) <- c("lm_full_aic", "lm_full_bic", "lm_red_aic", "lm_red_bic")
+
+lm_best = lm_full_aic
+## lm_full_aic is the best model. OBP was not selected for any models by exhaustive model selection.
+## Model: Salary ~ Hits + Triples + HomeRuns + StrikeOuts + StolenBases + Errors + FreeAgentEligibility + FreeAgent + ArbitrationEligibility + Arbitration
+## Categorical predictors have the highest values for the model. Arbitration eligibility has an extremely high impact, which makes sense
+## because salaries are determined through arbitration.
+
+####################################################################
+#### Final Model Plots/Tests
+####################################################################
+## Shapiro-Wilk
+shapiro_bwd = shapiro.test(lm_best$residuals)
+
+## R-plots
+plot(lm_best)
+plot(lm_best,which=c(4,6))
+
+means = data.frame(rbind(colMeans(baseball[1:17])))
+predict(lm_best,means, interval="predict")
 
