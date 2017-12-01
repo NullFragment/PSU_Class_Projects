ab0adbf20bdf90f2896c410da233374ef56a9192
Kyle Salitrik <nullfragment@gmail.com>
2017-12-01 14:54:55 -0500

    STAT462 - Project - Updated model selection removing outliers

--- STAT_462/project/project_log_transform.R
+++ STAT_462/project/project_log_transform.R
@@ -66,21 +66,21 @@ RSS_log_full = full_log_sum$rss
 
 ## Adjusted R2
 R2_adj_log_full = 1 - (RSS_log_full/(n - p_log_f))/(totalSS_log/(n - 1))
-plot(p_log_f, R2_adj_log_full, xlab = "Number of betas", ylab = "Adjusted R-squared")  # 7 betas / 6 predictors
+plot(p_log_f, R2_adj_log_full, xlab = "Number of betas", ylab = "Adjusted R-squared")
 
 ## Mallow's C
 C_p_log_full = RSS_log_full/(sigma_hat_log_full^2) + 2 * p_log_f - n
 C_p_diff_log_full = abs(abs(C_p_log_full) - p_log_f)
-plot(p_log_f, C_p_log_full, xlab = "Number of betas", ylab = "Mallow's Cp")  # 7 betas / 6 predictors
+plot(p_log_f, C_p_log_full, xlab = "Number of betas", ylab = "Mallow's Cp")
 abline(0, 1)
 
 ## AIC
 aic_log_full = n * log(RSS_log_full/n) + 2 * p_log_f
-plot(p_log_f, aic_log_full, xlab = "Number of betas", ylab = "AIC")  # 7 betas / 6 predictors
+plot(p_log_f, aic_log_full, xlab = "Number of betas", ylab = "AIC")
 
 ## BIC
 bic_log_full = n * log(RSS_log_full/n) + p_log_f * log(n)
-plot(p_log_f, bic_log_full, xlab = "Number of betas", ylab = "BIC")  # 7 betas / 6 predictors
+plot(p_log_f, bic_log_full, xlab = "Number of betas", ylab = "BIC")
 
 ## Suggested Number of Betas
 r2_log_f = which.max(R2_adj_log_full) + 1
@@ -99,21 +99,21 @@ RSS_log_red = red_log_sum$rss
 
 ## Adjusted R2
 R2_adj_red = 1 - (RSS_log_red/(n - p_log_r))/(totalSS_log/(n - 1))
-plot(p_log_r, R2_adj_red, xlab = "Number of betas", ylab = "Adjusted R-squared")  # 7 betas / 6 predictors
+plot(p_log_r, R2_adj_red, xlab = "Number of betas", ylab = "Adjusted R-squared")
 
 ## Mallow's C
 C_p_log_red = RSS_log_red/(sigma_hat_log_full^2) + 2 * p_log_r - n
 C_p_diff_log_red = abs(abs(C_p_log_red) - p_log_r)
-plot(p_log_r, C_p_log_red, xlab = "Number of betas", ylab = "Mallow's Cp")  # 7 betas / 6 predictors
+plot(p_log_r, C_p_log_red, xlab = "Number of betas", ylab = "Mallow's Cp")
 abline(0, 1)
 
 ## AIC
 aic_log_red = n * log(RSS_log_red/n) + 2 * p_log_r
-plot(p_log_r, aic_log_red, xlab = "Number of betas", ylab = "AIC")  # 7 betas / 6 predictors
+plot(p_log_r, aic_log_red, xlab = "Number of betas", ylab = "AIC")
 
 ## BIC
 bic_log_red = n * log(RSS_log_red/n) + p_log_r * log(n)
-plot(p_log_r, bic_log_red, xlab = "Number of betas", ylab = "BIC")  # 7 betas / 6 predictors
+plot(p_log_r, bic_log_red, xlab = "Number of betas", ylab = "BIC")
 
 
 ## Suggested Number of Betas
@@ -127,111 +127,68 @@ log_red_selection_betas = rbind(c(r2_log_r, mc_log_r, aic_log_r, bic_log_r))
 #### Model Selection Summary and VIF Analysis
 ####################################################################
 ## Number of Betas suggested by each method
-manual_log_both <- c(7, 7, 7, 7)  #Determined for full and reduced models
 
 selection_matrix_log_auto = matrix(rbind(log_full_selection_betas, log_red_selection_betas), 
                                    ncol = length(log_full_selection_betas))
-selection_matrix_log_user = matrix(rbind(manual_log_both, manual_log_both), 
-                                   ncol = length(manual_log_both))
 colnames(selection_matrix_log_auto) <- c("Adj R2", "MallowC", "AIC", "BIC")
 rownames(selection_matrix_log_auto) <- c("Full", "Reduced")
-colnames(selection_matrix_log_user) <- c("Adj R2", "MallowC", "AIC", "BIC")
-rownames(selection_matrix_log_user) <- c("Full", "Reduced")
 
-
-## NOTE: ALL MODELS BY MANUAL SELECTION ARE THE SAME AS AIC AND BIC.
-## Full Manual Selection Results
 #   (Intercept) BattingAverage   OBP  Runs  Hits Doubles Triples HomeRuns   RBI Walks StrikeOuts StolenBases Errors FreAgtEli FreeAgent ArbEli Arbitration
-#6         TRUE          FALSE FALSE  TRUE FALSE   FALSE   FALSE    FALSE  TRUE FALSE       TRUE       FALSE  FALSE      TRUE      TRUE   TRUE       FALSE
+#5         TRUE          FALSE FALSE FALSE  TRUE   FALSE   FALSE    FALSE  TRUE FALSE      FALSE       FALSE  FALSE      TRUE      TRUE   TRUE       FALSE
+#7         TRUE          FALSE FALSE FALSE  TRUE   FALSE   FALSE    FALSE  TRUE FALSE      FALSE        TRUE   TRUE      TRUE      TRUE   TRUE       FALSE
 
-## Reduced (No OBP) Manual Selection Results
 #   (Intercept) BattingAverage  Runs  Hits Doubles Triples HomeRuns   RBI Walks StrikeOuts StolenBases Errors FreAgtEli FreeAgent ArbEli Arbitration
-#6         TRUE          FALSE  TRUE FALSE   FALSE   FALSE    FALSE  TRUE FALSE       TRUE       FALSE  FALSE      TRUE      TRUE   TRUE       FALSE
+#5         TRUE          FALSE FALSE  TRUE   FALSE   FALSE    FALSE  TRUE FALSE      FALSE       FALSE  FALSE      TRUE      TRUE   TRUE       FALSE
+#7         TRUE          FALSE FALSE  TRUE   FALSE   FALSE    FALSE  TRUE FALSE      FALSE        TRUE   TRUE      TRUE      TRUE   TRUE       FALSE
 
-lm_log_user = lm(log(Salary) ~ Runs + RBI + StrikeOuts + FreAgtEli + FreeAgent + 
-                   ArbEli, data = baseball)
+## NOTE: Both models selected are the same for reduced and full!
 
-## Models with max R2 to see differences vs manual selection
-lm_log_full_r2 = lm(log(Salary) ~ OBP + Runs + Triples + RBI + StrikeOuts + 
-                      StolenBases + Errors + FreAgtEli + FreeAgent + ArbEli + Arbitration, 
-                    data = baseball)
-lm_log_red_r2 = lm(log(Salary) ~ BattingAverage + Runs + Hits + Triples + 
-                     RBI + StrikeOuts + StolenBases + Errors + FreAgtEli + FreeAgent + ArbEli, 
-                   data = baseball)
+lm_log_aic = lm(log(Salary) ~ Hits + RBI + FreAgtEli + FreeAgent + ArbEli, 
+                data = baseball)
+lm_log_bic = lm(log(Salary) ~ Hits + RBI + StolenBases + Errors + FreAgtEli + 
+                  FreeAgent + ArbEli, data = baseball)
 
 ######################
-# Full model
+# AIC model
 ######################
-vif(lm_log_full_r2)
-which.max(vif(lm_log_full_r2))
-
-lm_log_full_r2 = update(lm_log_full_r2, . ~ . - Runs)
-vif(lm_log_full_r2)
-which.max(vif(lm_log_full_r2))
+vif(lm_log_aic)
+which.max(vif(lm_log_aic))
 
 ######################
-# Reduced model
+# BIC model
 ######################
-vif(lm_log_red_r2)
-which.max(vif(lm_log_red_r2))
+vif(lm_log_bic)
+which.max(vif(lm_log_bic))
 
-lm_log_red_r2 = update(lm_log_red_r2, . ~ . - Hits)
-vif(lm_log_red_r2)
-which.max(vif(lm_log_red_r2))
-
-lm_log_red_r2 = update(lm_log_red_r2, . ~ . - Runs)
-vif(lm_log_red_r2)
-which.max(vif(lm_log_red_r2))
-
-######################
-# Manual model
-######################
-vif(lm_log_user)
-which.max(vif(lm_log_user))
-
-lm_log_user = update(lm_log_user, . ~ . - RBI)
-vif(lm_log_user)
-which.max(vif(lm_log_user))
-
+lm_log_bic = update(lm_log_bic, . ~ . - Hits)
+vif(lm_log_bic)
+which.max(vif(lm_log_bic))
 
 ####################################################################
 #### Adjusted Models after VIF and final model selection
 ####################################################################
 ## After selection lm_red_r2 and lm_full_bic are the same
-summary(lm_log_full_r2)  # Adj R2 Value = 0.7912
-summary(lm_log_red_r2)  # Adj R2 Value = 0.7918
-summary(lm_log_user)  # Adj R2 Value = 0.7816
+summary(lm_log_aic)  # Adj R2 Value = 0.7912
+summary(lm_log_bic)  # Adj R2 Value = 0.7918
 
-adj_r2_sum = c(summary(lm_log_full_r2)$adj.r.squared, summary(lm_log_red_r2)$adj.r.squared, 
-               summary(lm_log_user)$adj.r.squared)
-names(adj_r2_sum) <- c("lm_log_full_r2", "lm_log_red_r2", "lm_log_user")
-
-#lm_log_full_r2  lm_log_red_r2    lm_log_user 
-#     0.7912379      0.7918032      0.7815753 
+adj_r2_sum = c(summary(lm_log_aic)$adj.r.squared, summary(lm_log_bic)$adj.r.squared)
+names(adj_r2_sum) <- c("lm_log_aic", "lm_log_bic")
 
 ####################################################################
 #### Final Model Plots/Tests
 ####################################################################
 ## Full Adj-R^2 Selected Model
-plot(lm_log_full_r2, which = 1)
-plot(lm_log_full_r2, which = 2)
-plot(lm_log_full_r2, which = 3)
-plot(lm_log_full_r2, which = 4)
-plot(lm_log_full_r2, which = 5)
-shapiro.test(lm_log_full_r2$residuals)
+plot(lm_log_aic, which = 1)
+plot(lm_log_aic, which = 2)
+plot(lm_log_aic, which = 3)
+plot(lm_log_aic, which = 4)
+plot(lm_log_aic, which = 5)
+shapiro.test(lm_log_aic$residuals)
 
 ## Reduced Adj-R^2 Selected Model
-plot(lm_log_red_r2, which = 1)
-plot(lm_log_red_r2, which = 2)
-plot(lm_log_red_r2, which = 3)
-plot(lm_log_red_r2, which = 4)
-plot(lm_log_red_r2, which = 5)
-shapiro.test(lm_log_red_r2$residuals)
-
-## Manually Selected Model
-plot(lm_log_user, which = 1)
-plot(lm_log_user, which = 2)
-plot(lm_log_user, which = 3)
-plot(lm_log_user, which = 4)
-plot(lm_log_user, which = 5)
-shapiro.test(lm_log_user$residuals)
+plot(lm_log_bic, which = 1)
+plot(lm_log_bic, which = 2)
+plot(lm_log_bic, which = 3)
+plot(lm_log_bic, which = 4)
+plot(lm_log_bic, which = 5)
+shapiro.test(lm_log_bic$residuals)
