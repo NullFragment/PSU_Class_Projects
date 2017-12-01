4d1a0a82888da33b3c1f5bb2051ff7b44b656095
Kyle Salitrik <nullfragment@gmail.com>
2017-12-01 01:22:07 -0500

    CMPSC431 - Design 7 - Design doc corrections

--- CMPSC_431W/Design_Assignment_5/code/generator.py
+++ CMPSC_431W/Design_Assignment_5/code/generator.py
@@ -1,8 +1,11 @@
 from random import randint, sample
-
+from datetime import datetime, timedelta
 print("INSERT INTO cmpsc431.department_need (week_ID, day_ID, time_ID, dept_ID, role_ID, need) VALUES ")
+start = datetime.strptime('20171001', '%Y%m%d').date()
+addition = -1;
 for week_ID in range(1, 13):
     for day_ID in range(1, 8):
+        addition = addition+1;
         if day_ID == 1 or day_ID == 7:
             for time_ID in range(4, 6):
                 for dept_ID in range(1, 7):
@@ -13,7 +16,8 @@ for week_ID in range(1, 13):
                               + str(time_ID) + ", "
                               + str(dept_ID) + ", "
                               + str(role_ID) + ", "
-                              + str(randint(2, 3)) + "),")
+                              + str(randint(2, 3)) + ","
+                              + "'" + str(start + timedelta(days=addition)) + "'),")
         else:
             for time_ID in range(1, 4):
                 for dept_ID in range(1, 7):
@@ -24,12 +28,15 @@ for week_ID in range(1, 13):
                               + str(time_ID) + ", "
                               + str(dept_ID) + ", "
                               + str(role_ID) + ", "
-                              + str(randint(2, 3)) + "),")
+                              + str(randint(2, 3)) + ","
+                              + "'" + str(start + timedelta(days=addition)) + "'),")
 
 print("INSERT INTO cmpsc431.shift (emp_ID, dept_ID, time_ID, week_ID, day_ID, status_ID, pay_modifier) VALUES ")
 for emp_ID in range(1, 21):
     for week_ID in range(1, 13):
+        addition = (week_ID - 1) * 7;
         for day_ID in sample(range(1, 8), 5):
+            addition2 = addition + (day_ID - 1);
             pay_modifier = 1.0
             if day_ID == 1 or day_ID == 7:
                 time_ID = randint(4, 5)
@@ -50,4 +57,5 @@ for emp_ID in range(1, 21):
                   + str(week_ID) + ", "
                   + str(day_ID) + ", "
                   + str(status_ID) + ", "
-                  + str(pay_modifier) + "),")
+                  + str(pay_modifier) + ", "
+                  + "'" + str(start + timedelta(days=addition2)) + "'),")
