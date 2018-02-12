ea4942ab4351bf50ee99ee01a56b77e7feb3eaf9
Kyle Salitrik <nullfragment@gmail.com>
2018-02-12 18:11:56 -0500

    CMPSC448 - HW1 - Added extra function for q1. Still failing

--- CMPSC_448/HW_1/homework1.py
+++ CMPSC_448/HW_1/homework1.py
@@ -1,8 +1,9 @@
 import numpy as np
 
 
-def findLongestSequence(array_in):
+def findLongestInitialSequence(array_in):
     current_element = array_in[0]
+    to_match = current_element
     longest_sequence = -1
     current_sequence = 1
 
@@ -12,8 +13,27 @@ def findLongestSequence(array_in):
         else:
             if current_sequence > longest_sequence:
                 longest_sequence = current_sequence
+                to_match = current_element
             current_sequence = 1
         current_element = array_in[i]
+
+    if current_sequence > longest_sequence:
+        longest_sequence = current_sequence
+
+    return longest_sequence, to_match
+
+
+def findLongestSequence(array_in, to_match):
+    longest_sequence = -1
+    current_sequence = 0
+
+    for i in range(0, len(array_in)):
+        if array_in[i] == to_match:
+            current_sequence += 1
+        else:
+            if current_sequence > longest_sequence:
+                longest_sequence = current_sequence
+            current_sequence = 0
 
     if current_sequence > longest_sequence:
         longest_sequence = current_sequence
@@ -23,19 +43,19 @@ def findLongestSequence(array_in):
 
 # Question 1
 def q1(history):
-    iter = 1000000
-    longest_in = findLongestSequence(history)
+    iterations = 10000
+    longest_in, char_to_match = findLongestInitialSequence(history)
     print(longest_in)
     equal_length_successes = 0
 
-    for x in range(0, iter):
+    for x in range(0, iterations):
         np.random.shuffle(history)
-        longest_seq = findLongestSequence(history)
+        longest_seq = findLongestSequence(history, char_to_match)
         if longest_seq >= longest_in:
             equal_length_successes += 1
 
-    return (equal_length_successes / iter)
-
+    return (equal_length_successes / iterations)
+    
 
 # Question 2
 def q2(p):
@@ -45,3 +65,4 @@ def q2(p):
 # Question 3
 def q3(N):
     None
+
