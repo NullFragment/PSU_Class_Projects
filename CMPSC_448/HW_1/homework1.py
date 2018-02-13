3fbd933a47ea79ad5a55fe8518b33401f0b26d5c
Kyle Salitrik <nullfragment@gmail.com>
2018-02-12 19:58:32 -0500

    CMPSC448 - HW1 - Updated 3rd and 1st question

--- CMPSC_448/HW_1/homework1.py
+++ CMPSC_448/HW_1/homework1.py
@@ -1,9 +1,8 @@
 import numpy as np
 
 
-def findLongestInitialSequence(array_in):
+def findLongestSequence(array_in):
     current_element = array_in[0]
-    to_match = current_element
     longest_sequence = -1
     current_sequence = 1
 
@@ -13,27 +12,8 @@ def findLongestInitialSequence(array_in):
         else:
             if current_sequence > longest_sequence:
                 longest_sequence = current_sequence
-                to_match = current_element
             current_sequence = 1
-        current_element = array_in[i]
-
-    if current_sequence > longest_sequence:
-        longest_sequence = current_sequence
-
-    return longest_sequence, to_match
-
-
-def findLongestSequence(array_in, to_match):
-    longest_sequence = -1
-    current_sequence = 0
-
-    for i in range(0, len(array_in)):
-        if array_in[i] == to_match:
-            current_sequence += 1
-        else:
-            if current_sequence > longest_sequence:
-                longest_sequence = current_sequence
-            current_sequence = 0
+            current_element = array_in[i]
 
     if current_sequence > longest_sequence:
         longest_sequence = current_sequence
@@ -43,26 +23,52 @@ def findLongestSequence(array_in, to_match):
 
 # Question 1
 def q1(history):
-    iterations = 10000
-    longest_in, char_to_match = findLongestInitialSequence(history)
-    print(longest_in)
+
+    iterations = 10000000
+    longest_in = findLongestSequence(history)
     equal_length_successes = 0
 
     for x in range(0, iterations):
         np.random.shuffle(history)
-        longest_seq = findLongestSequence(history, char_to_match)
+        longest_seq = findLongestSequence(history)
         if longest_seq >= longest_in:
             equal_length_successes += 1
 
     return (equal_length_successes / iterations)
-    
+
 
 # Question 2
 def q2(p):
-    None
+    return(0)
 
 
 # Question 3
 def q3(N):
-    None
+    assert (N % 2 == 0)
+    seasons = 1000000
+    pairs = N // 2
+    teams = list(range(0, N))
+    game_probabilities = [0.5] * pairs
 
+    season_winners = [0] * seasons
+
+    for season in range(0, seasons):
+        wins = [0] * N
+
+        for game in range(0, 16):
+            np.random.shuffle(teams)
+            winners = np.random.binomial(1, game_probabilities)
+
+            for pair in range(0, pairs):
+                if winners[pair] > 0.5:
+                    wins[teams[pair * 2]] += 1
+                else:
+                    wins[teams[pair * 2 + 1]] += 1
+
+        season_winners[season] = max(wins)
+
+    return np.mean(season_winners)
+
+
+print(q1([1, 1, 1, 0, 0, 0, 1, 0, 1, 0]))
+print(q1([1, 1, 0, 0, 1, 1, 0, 0, 1, 1]))
