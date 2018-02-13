74f98ecfd64381ed4d20b0e0e26be5f968a5b25f
Kyle Salitrik <nullfragment@gmail.com>
2018-02-12 22:08:35 -0500

    CMPSC448 - HW1 - Final

--- CMPSC_448/HW_1/homework1.py
+++ CMPSC_448/HW_1/homework1.py
@@ -1,74 +1,100 @@
 import numpy as np
 
+# set number of iterations for all simulations:
+iterations = 100000
 
-def findLongestSequence(array_in):
-    current_element = array_in[0]
-    longest_sequence = -1
-    current_sequence = 1
 
-    for i in range(1, len(array_in)):
-        if array_in[i] == current_element:
-            current_sequence += 1
-        else:
-            if current_sequence > longest_sequence:
-                longest_sequence = current_sequence
-            current_sequence = 1
-            current_element = array_in[i]
+def findPairedValues(array_in):
+    pairs = 0
 
-    if current_sequence > longest_sequence:
-        longest_sequence = current_sequence
+    # Simply iterate through history and find number of paired values
+    for item in range(1, len(array_in)):
+        if array_in[item] == array_in[item - 1]:
+            pairs += 1
 
-    return longest_sequence
+    return pairs
 
 
 # Question 1
 def q1(history):
-
-    iterations = 10000000
-    longest_in = findLongestSequence(history)
-    equal_length_successes = 0
+    initial_pairs = findPairedValues(history)  # Store number of paired values to compare to for test statistic
+    test_stat = 0
 
     for x in range(0, iterations):
-        np.random.shuffle(history)
-        longest_seq = findLongestSequence(history)
-        if longest_seq >= longest_in:
-            equal_length_successes += 1
+        np.random.shuffle(history)  # Permute history
+        pairs = findPairedValues(history)  # Find number of paired values in permutation
+        if pairs >= initial_pairs:
+            test_stat += 1  # Increment test statistic if success
 
-    return (equal_length_successes / iterations)
+    return test_stat / iterations  # Return p-value
 
 
 # Question 2
 def q2(p):
-    return(0)
+    # This is the result of solving for alpha (a) in the following costs:
+    # Cost(treating, type A)*P(A) + Cost(treating, B)*P(B) = Cost(no treatment, A)*P(A) + Cost(no treatment, B)*P(B)
+    # a*p + 1*(1-p) = 2*p + 5*(1-p)
+    # a*p = 2*p + 4*(1-p)
+    # a = [2*p + 4*(1-p)]/p
+
+    return (4 * (1 - p) + 2 * p) / (p)
 
 
 # Question 3
 def q3(N):
+    # Asserting that the teams must be even. According to our test statistic this must be true because each team
+    # must play every day of the season.
     assert (N % 2 == 0)
-    seasons = 1000000
-    pairs = N // 2
+
+    # Set up constants for iteration.
+    seasons = iterations  # Determines number of iterations to run simulation
+    pairs = N // 2  # Used to cut down on integer division later
+    game_probabilities = [0.5] * pairs  # Sets probabilities to draw binomial distribution in loop
+
+    # Set up space to store values
     teams = list(range(0, N))
-    game_probabilities = [0.5] * pairs
+    total_champion_wins = 0
 
-    season_winners = [0] * seasons
-
+    # Iterate over all "seasons" to be simulated
     for season in range(0, seasons):
-        wins = [0] * N
+        wins = [0] * N  # Reset wins vector each season
 
-        for game in range(0, 16):
+        # Iterate over all 16 games in season
+        for day in range(0, 16):
+            # Shuffle teams playing against each other for each day
             np.random.shuffle(teams)
+
+            # Draw winner for games from Bernoulli Distribution: 1 = first team wins, 0 = second team wins
             winners = np.random.binomial(1, game_probabilities)
 
+            # Iterate over each pair of teams (N/2 games per day for each team)
             for pair in range(0, pairs):
-                if winners[pair] > 0.5:
-                    wins[teams[pair * 2]] += 1
+                if winners[pair] == 1:
+                    wins[teams[pair * 2]] += 1  # Adds a win into the win vector for team 1 of the game
                 else:
-                    wins[teams[pair * 2 + 1]] += 1
+                    wins[teams[pair * 2 + 1]] += 1  # Adds a win into the win vector for team 2 of the game
 
-        season_winners[season] = max(wins)
+        total_champion_wins += max(wins)  # Find the team with the max wins and add it to the total
 
-    return np.mean(season_winners)
+    return total_champion_wins / seasons
 
+## Unused function from misinterpreting homework statement:
 
-print(q1([1, 1, 1, 0, 0, 0, 1, 0, 1, 0]))
-print(q1([1, 1, 0, 0, 1, 1, 0, 0, 1, 1]))
+# def findLongestSequence(array_in):
+#     current_element = array_in[0]
+#     longest_sequence = -1
+#     current_sequence = 1
+#
+#     for i in range(1, len(array_in)):
+#         if array_in[i] == current_element:
+#             current_sequence += 1
+#         else:
+#             if current_sequence > longest_sequence:
+#                 longest_sequence = current_sequence
+#             current_sequence = 1
+#             current_element = array_in[i]
+#
+#     if current_sequence > longest_sequence:
+#         longest_sequence = current_sequence
+#
+#     return longest_sequence
--- CMPSC_448/HW_1/test_homework1.py
+++ CMPSC_448/HW_1/test_homework1.py
@@ -14,16 +14,14 @@ f.close()
 
 @pytest.mark.parametrize('inp, expected', [i for i in tests['q1']])
 def test_q1(inp, expected):
-    blah = q1(inp)
-    print(blah)
-    assert blah == pytest.approx(expected, 0.005)
+    assert q1(inp) == pytest.approx(expected, 0.005)
 
 
-# @pytest.mark.parametrize('inp, expected', [i for i in tests['q2']])
-# def test_q2(inp, expected):
-#     assert q2(inp) == pytest.approx(expected, 0.0000005)
-#
-#
-# @pytest.mark.parametrize('inp, expected', [i for i in tests['q3']])
-# def test_q3(inp, expected):
-#     assert q3(inp) == pytest.approx(expected, 0.0005)
+@pytest.mark.parametrize('inp, expected', [i for i in tests['q2']])
+def test_q2(inp, expected):
+    assert q2(inp) == pytest.approx(expected, 0.0000005)
+
+
+@pytest.mark.parametrize('inp, expected', [i for i in tests['q3']])
+def test_q3(inp, expected):
+    assert q3(inp) == pytest.approx(expected, 0.0005)
