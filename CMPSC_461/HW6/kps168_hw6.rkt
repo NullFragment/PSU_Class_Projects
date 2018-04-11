fb523ee7eab1fc6eabcd3203ac7559e15c4800c2
Kyle Salitrik <nullfragment@gmail.com>
2018-04-11 15:53:24 -0400

    CMPSC461 - HW6 - Started

---
+++ CMPSC_461/HW6/kps168_hw6.rkt
@@ -0,0 +1,56 @@
+#|
+    WRITTEN BY: Kyle Salitrik
+    PSU ID: kps168
+    ASSIGNMENT: HW 6
+|#
+
+
+#| GLOBALLY USED FUNCTIONS |#
+(define (bind k v assoc_list) (cons (list k v) assoc_list))
+(define (lookup k assoc_list)
+  (cond
+    ((null? assoc_list) #f)
+    ((equal? (caar assoc_list) k) (car assoc_list))
+    (else (lookup k (cdr assoc_list)))))
+
+
+#| PROBLEM 1 |#
+(define al '())
+
+(define (fac x)
+  (cond
+    ((= x 1) 1)
+    (else (* x (fac (- x 1))))))
+
+(define (fac_mem x)
+  (let ((res (lookup x al)))
+    (cond
+      ((equal? res #f) (begin (set! al (bind x (fac x) al))) (fac x))
+      (else (begin (display "memoization hit: ")) (cadr res))
+      )))
+
+
+#| PROBLEM 2 |# 
+(define (build_mem f)
+  (let ((local_al '()))
+    (lambda (n)
+      (let ((res (lookup n local_al)))
+        (cond
+          ((equal? res #f) (begin (set! local_al (bind n (f n) local_al))) (f n))
+          (else (begin (display "memoization hit: ")) (cadr res)))))))
+
+
+#|
+(define (fac_mem x)
+  (let ((res (lookup x al)))
+  (cond
+    ((equal? res #f) res)
+    (else (cadr res)))))
+(define (fac x)
+  (let ((res (fac_mem x)))
+  (cond
+    ((and (equal? res #f) (equal? x 1)) (bind 1 1 al) 1)
+    ((equal? res #f) (begin (bind x (* x (fac (- x 1))) al)) (fac_mem x))
+    (else (begin (display "memo hit: ")) res))))
+
+|#
